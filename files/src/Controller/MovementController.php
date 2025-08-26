f4a8fd7f8fd7b1c655e21a8ead6636cf9bf92f3d
alessandro <alessandro@Ubuntu.myguest.virtualbox.org>
2025-08-26 15:40:24 +0200

    Modifiche al Controller e aggiunto metodo per ordinamento movimenti

--- files/src/Controller/MovementController.php
+++ files/src/Controller/MovementController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use App\Entity\Movement;
 use App\Form\MovementType;
 use App\Enum\MovementType as MT;
+use App\Repository\CategoryRepository;
+use App\Repository\MovementRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,11 +15,29 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/movimenti')]
 class MovementController extends AbstractController
 {
+    #[Route('/', name: 'movement_index', methods: ['GET'])]
+    public function index(
+        Request $request,
+        MovementRepository $movementRepo,
+        CategoryRepository $categoryRepo
+    ): Response {
+        $categoryId = $request->query->get('category'); // null o id
+        $category = $categoryId ? $categoryRepo->find($categoryId) : null;
+
+        $movements = $movementRepo->findByFilter(category: $category);
+
+        return $this->render('movement/index.html.twig', [
+            'movements' => $movements,
+            'categories' => $categoryRepo->findBy([], ['name' => 'ASC']),
+            'selectedCategory' => $category?->getId(),
+        ]);
+    }
+
     #[Route('/nuovo', name: 'movement_new', methods: ['GET','POST'])]
     public function new(Request $request, EntityManagerInterface $em): Response
     {
         $movement = new Movement();
-        $movement->setType(MT::EXPENSE); // default UI: Uscita (così mostra la categoria)
+        $movement->setType(MT::EXPENSE);
 
         $form = $this->createForm(MovementType::class, $movement);
         $form->handleRequest($request);
@@ -27,11 +47,45 @@ class MovementController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'Movimento salvato con successo.');
-            return $this->redirectToRoute('movement_new'); // resta sulla pagina di inserimento
+            // ➜ dopo il salvataggio, vai al riepilogo
+            return $this->redirectToRoute('movement_index');
         }
 
         return $this->render('movement/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/{id}/modifica', name: 'movement_edit', methods: ['GET','POST'])]
+    public function edit(Movement $movement, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(MovementType::class, $movement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Movimento aggiornato.');
+            return $this->redirectToRoute('movement_index');
+        }
+
+        return $this->render('movement/edit.html.twig', [
+            'form' => $form->createView(),
+            'movement' => $movement,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'movement_delete', methods: ['POST'])]
+    public function delete(Movement $movement, Request $request, EntityManagerInterface $em): Response
+    {
+        $csrfId = 'delete_movement_'.$movement->getId();
+        if ($this->isCsrfTokenValid($csrfId, $request->request->get('_token'))) {
+            $em->remove($movement);
+            $em->flush();
+            $this->addFlash('success', 'Movimento eliminato.');
+        } else {
+            $this->addFlash('danger', 'Token CSRF non valido. Riprova.');
+        }
+
+        return $this->redirectToRoute('movement_index');
     }
 }
--- files/src/Repository/MovementRepository.php
+++ files/src/Repository/MovementRepository.php
@@ -1,18 +1,32 @@
 <?php
-
 namespace App\Repository;
 
+use App\Entity\Category;
 use App\Entity\Movement;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Movement>
- */
 class MovementRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Movement::class);
+    }
+
+    /**
+     * @return Movement[]
+     */
+    public function findByFilter(?Category $category = null): array
+    {
+        $qb = $this->createQueryBuilder('m')
+            ->leftJoin('m.category', 'c')->addSelect('c')
+            ->orderBy('m.date', 'DESC')
+            ->addOrderBy('m.id', 'DESC');
+
+        if ($category) {
+            $qb->andWhere('m.category = :cat')->setParameter('cat', $category);
+        }
+
+        return $qb->getQuery()->getResult();
     }
 }
