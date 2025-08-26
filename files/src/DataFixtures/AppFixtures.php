236b2fe00a14872650d5ed65b3546eb97eb9cc02
alessandro <alessandro@Ubuntu.myguest.virtualbox.org>
2025-08-26 14:38:53 +0200

    aggiunta di fixture opzionali e link di navigazione alla pagina di aggiunta movimento

--- files/src/DataFixtures/AppFixtures.php
+++ files/src/DataFixtures/AppFixtures.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +9,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach (['Spesa', 'Affitto', 'Trasporti', 'Utenze', 'Svago'] as $name) {
+            $c = new Category();
+            $c->setName($name);
+            $manager->persist($c);
+        }
         $manager->flush();
     }
 }
